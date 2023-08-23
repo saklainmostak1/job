ed545f7ee1eda9c9fc26a4ac822c43516c890b0a
saklainmostak1 <saklain15-12003@diu.edu.bd>
2023-08-23 04:32:39 -0700

    push

--- src/Connection/database.js
+++ src/Connection/database.js
@@ -4,25 +4,24 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5002
 const cors = require('cors')
-// const bodyParsar = require("body-parser")
-
-
+const bodyParsar = require("body-parser")
 
 app.use(cors())
-// app.use(bodyParsar.json())
-// app.use(express.json())
+app.use(bodyParsar.json())
+app.use(express.json())
 
 const db = require('./connection')
  const admin_page_list_all = require('../Model/Admin_Model/Admin_page_list_Model')
 
-console.log(admin_page_list_all, 'api is rumming')
+
 
 // const router = require('../Model/Admin_Model/Admin_page_list_Model')
 
 
 
 
-app.use('/nayan', admin_page_list_all)
+
+// app.use('/all-admin', admin_page_list_all)
 
 
 // db.connect(function(error){
--- src/Model/Admin_Model/Admin_page_list_Model.js
+++ src/Model/Admin_Model/Admin_page_list_Model.js
@@ -1,52 +1,54 @@
 const db = require('../../Connection/connection')
 
+const express = require('express'),
+router = express.Router()
+
 
  function admin_page_list_all(req, res) {
     const data = "select * from 	admin_page_list";
     db.query(data, function (error, result) {
         if (error) throw error;
-        res.json(result)
-        console.log(result, 'nayan')
+        return res.json(result)
     })
 }
-//  function admin_page_list_create(req, res) {
-//     const data = "select * from 	admin_page_list";
-//     db.query(data, function (error, result) {
-//         if (error) throw error;
-//         return res.json(result)
-//     })
-// }
-//  function admin_page_list_edit(req, res) {
-//     const data = "select * from 	admin_page_list";
-//     db.query(data, function (error, result) {
-//         if (error) throw error;
-//         return res.json(result)
-//     })
-// }
-//  function admin_page_list_delete(req, res) {
-//     const data = "select * from 	admin_page_list";
-//     db.query(data, function (error, result) {
-//         if (error) throw error;
-//         return res.json(result)
-//     })
-// }
+ function admin_page_list_create(req, res) {
+    const data = "select * from 	admin_page_list";
+    db.query(data, function (error, result) {
+        if (error) throw error;
+        return res.json(result)
+    })
+}
+ function admin_page_list_edit(req, res) {
+    const data = "select * from 	admin_page_list";
+    db.query(data, function (error, result) {
+        if (error) throw error;
+        return res.json(result)
+    })
+}
+ function admin_page_list_delete(req, res) {
+    const data = "select * from 	admin_page_list";
+    db.query(data, function (error, result) {
+        if (error) throw error;
+        return res.json(result)
+    })
+}
 
-//  function admin_page_list_single(req, res) {
-//     const data = "select * from 	admin_page_list";
-//     db.query(data, function (error, result) {
-//         if (error) throw error;
-//         return res.json(result)
-//     })
-// }
+ function admin_page_list_single(req, res) {
+    const data = "select * from 	admin_page_list";
+    db.query(data, function (error, result) {
+        if (error) throw error;
+        return res.json(result)
+    })
+}
 
 module.exports = 
 {
     admin_page_list_all,
-    // admin_page_list_single,
-    // admin_page_list_create,
-    // admin_page_list_edit,
-    // admin_page_list_delete,
-
+    admin_page_list_single,
+    admin_page_list_create,
+    admin_page_list_edit,
+    admin_page_list_delete,
+    router
 }
 
 // export default
